d778941320d7302802e952fd4bbf1dad634d2d08
Diego Santos <diegoleonardo@outlook.com>
2017-12-10 11:26:31 -0200

    Teste deploy php app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,18 +31,15 @@
     <script>
 
         var connection = new WebSocket('ws://localhost:4000/socket/websocket');
-
+        
         // When the connection is open, send some data to the server
         connection.onopen = function () {
-            console.log("Conexão aberta");
-            
-            connection.send('{"topic":"room:tracking","ref":"1","payload":{},"event":"phx_join"}'); // Send the message 'Ping' to the server
+           
+            connection.send('{"topic":"room:tracking","ref":"1","payload":{},"event":"phx_join"}');
             
             if(localStorage.tracking){
-                console.log("Já está no tracking")
                 connection.send(`{"topic":"room:tracking","ref":"","payload":{"id":${localStorage.tracking},"url": "${window.location.href}", "accessDate": "${new Date().toJSON()}"},"event":"tracking_suspect"}`);
             }else{
-                console.log("Não está no tracking")
                 connection.send(`{"topic":"room:tracking","ref":"","payload":{"url": "${window.location.href}", "accessDate": "${new Date().toJSON()}"},"event":"tracking_suspect"}`);
             }
         };
@@ -61,31 +58,7 @@
                     localStorage.setItem("tracking", `${json.payload.response.id}`);
                 }
             }
-
-            console.log('Server: ' + e.data);
         };
-
-        // Now that you are connected, you can join channels with a topic:
-        {{--  let channel           = socket.channel("room:lobby", {})
-        let chatInput         = document.querySelector("#chat-input")
-        let messagesContainer = document.querySelector("#messages")
-
-        chatInput.addEventListener("keypress", event => {
-            if(event.keyCode === 13){
-                channel.push("new_msg", {body: chatInput.value})
-                chatInput.value = ""
-            }
-        })
-
-        channel.on("new_msg", paylod =>{
-            let messagemItem = document.createElement("li");
-            messagemItem.innerText = `[${Date()}] ${paylod.body}`
-            messagesContainer.appendChild(messagemItem)
-        })
-
-        channel.join()
-            .receive("ok", resp => { console.log("Joined successfully", resp) })
-            .receive("error", resp => { console.log("Unable to join", resp) })  --}}
 
     </script>
 </body>
